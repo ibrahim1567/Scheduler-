08edc9ebcdc6d620a08d40ecc1a88b06a2e9fc9d
Ibrahim Mohamed <ia.mohamed02@gmail.com>
2022-03-02 00:13:10 -0500

    refactoring interviewer components

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import "components/InterviewerList.scss";
+import InterviewerListItem from './InterviewerListItem';
+
+export default function InterviewerList (props) {
+  return (
+  <section className="interviewers">
+  <h4 className="interviewers__header text--light">Interviewer</h4>
+  <ul className="interviewers__list">
+    {props.interviewers.map((interview) =>{
+      return (
+        <InterviewerListItem
+        id={interview.id}
+        name={interview.name}
+        avatar={interview.avatar}
+        setInterviewer={props.setInterviewer}
+        selected={interview.id === props.interviewer}
+        />
+
+      )
+    })}
+  </ul>
+</section>
+);
+
+}  
