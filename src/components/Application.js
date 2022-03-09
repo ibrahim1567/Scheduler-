da0d8e86ae95cb284ddd45b77939db2dd4f3d887
Ibrahim Mohamed <ia.mohamed02@gmail.com>
2022-03-08 23:39:10 -0500

    adding hooks

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,14 +1,11 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
-
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment"
+import {getAppointmentsForDay, getInterview}  from  "../helpers/selectors"
 
-
-import {getAppointmentsForDay,}  from  "../helpers/selectors"
-
-export default function Application(props) {
+export default function Application() {
 
   const [state, setState] = useState({
     day: "Monday",
@@ -22,24 +19,29 @@ export default function Application(props) {
   
     useEffect(() => {
      
-    let promiseDays = axios.get(`/api/days`);
-    let promiseAppointments = axios.get(`/api/appointments`);
-    let promiseInterviewers = axios.get(`/api/interviewers`);
+      let daysUrl = axios.get(`/api/days`);
+      let appointmentsUrl = axios.get(`/api/appointments`);
+      let interviewerUrl = axios.get(`/api/interviewers`);
 
 
     Promise.all([
-      promiseDays,
-      promiseAppointments,
-      promiseInterviewers
-    ]).then((all) => {
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
+      daysUrl,
+      appointmentsUrl,
+      interviewerUrl
+    ]).then((response) => {
+      const [daysData, appointmentData, interviewerData] = response
+      setState(prev => ({ ...prev, days: daysData.data, appointments: appointmentData.data, interviewers: interviewerData.data}));
     });
   },[]);
     console.log("the interviewer data",state.interviewers)
     const filteredAppointments = getAppointmentsForDay(state, state.day);
     const appointmentList = filteredAppointments.map(appointment => {
 
-    return <Appointment  time={appointment.time} key={appointment.id} {...appointment} />;
+      const interview = getInterview(state, appointment.interview);
+      return <Appointment    key={appointment.id}
+      id={appointment.id}
+      time={appointment.time}
+      interview={interview} />;
 
     })
   
